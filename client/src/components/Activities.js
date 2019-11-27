dc8de21d6f55d31db684d78753e09729d6e3d85c
amartignago <martignagoagostina@gmail.com>
2019-11-26 22:13:35 -0300

    fetchActivitiesV2

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,61 +1,53 @@
-import React, {Component, Fragment} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import urlImages from './constants';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {fetchActivities} from '../actions/activitiesActions';
+
 
 class Activities extends Component {
-    constructor(props) {
-        super(props) 
-        this.state = {
-          }
-      };
-      
-    static propTypes = {
-    dispatch: PropTypes.func,
+  constructor(props) {
+      super(props) 
+      this.state = {
+        }
     };
+    
+  static propTypes = {
+  dispatch: PropTypes.func,
+  };
 
-    componentDidMount() {
-        let id = this.props.itinerary._id;
-        console.log(id);
-        this.props.dispatch(fetchActivities(id))
-       .then(() => console.log('working'))
-    };
-
-
-    render() { 
-        const activities = this.props.activities; 
-        
-        return ( <Fragment>
-        <div className="d-flex mt-2 container h-70">
-            <h4 className="text-center mb-4 itinText font-weight-bold">Activities:</h4>
-            <Carousel className="mt-5 mb-3">
-                {activities.map(activity => 
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100 itinText"
-                                src={`${urlImages.urlImages}/images/activities/${activity.photo}`}
-                                alt={activity.name}
-                            />
-                            <Carousel.Caption>
-                                <h3 className="text-dark">{activity.name}</h3>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                )}
-            </Carousel> 
-        </div>
-    </Fragment>
+  render() { 
+    // const activities = this.props.activities; 
+    
+    return ( <Fragment>
+        <h4 className="text-left m-4 itinText font-weight-bold">Activities:</h4>
+        <div className="d-flex container-fluid justify-content-center">
+        <Carousel className="mt-5 mb-3">
+            {this.props.activities.map(activity => 
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 itinText CarouselImg"
+                            src={`${urlImages.urlImages}/images/activities/${activity.photo}`}
+                            alt={activity.name}
+                        />
+                        <Carousel.Caption>
+                            <h3 className="text-dark carouselCaption">{activity.name}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+            )}
+        </Carousel> 
+    </div>
+</Fragment>
 );
 }
 }
 
 const mapStateToProps = (state) => { 
-    console.log(state);
-    return {
-        itineraries: state.itinerariesReducer.itineraries,
-        activities: state.activitiesReducer.activities
-    }
+  console.log(state);
+  return {
+      activities: state.activitiesReducer.activities
+  }
 }
 
 export default connect(mapStateToProps)(Activities); // 
+
--- client/src/components/Itineraries.js
+++ client/src/components/Itineraries.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar.js'
 import '../styles/App.css'
 import {connect} from 'react-redux';
 import {fetchItineraries} from '../actions/itinerariesActions';
+import {fetchActivities} from '../actions/activitiesActions';
 import PropTypes from 'prop-types';
 import Expand from 'react-expand-animated';
 import Activities from './Activities.js';
@@ -17,29 +18,45 @@ class Itineraries extends Component {
         super(props)
         this.state = {
             open:false,
-            clicked: false
+            clicked: false,
+            cities: [],
+            filteredCities: []
         }
     };
     static propTypes = {
         dispatch: PropTypes.func,
       };
 
-    toggle = (id) => {
-        this.setState(() => ({ open: id, clicked: !this.state.clicked }));
+    toggle = (itinID) => {
+        this.setState(() => ({ open: itinID, clicked: !this.state.clicked }));
+        this.props.dispatch(fetchActivities(itinID));
     }
 
     componentDidMount() {
-        let id = this.props.match.params.cityID;
-        console.log(id);
-        this.props.dispatch(fetchItineraries(id))
+        let cityID = this.props.match.params.cityID;
+        console.log(cityID);
+        this.props.dispatch(fetchItineraries(cityID))
        .then(() => console.log('hola, funciono!'))
     };
+
+    // filterCities = (citiesFilter) => {
+    //     let filteredCities = this.state.cities
+    //     filteredCities = filteredCities.filter((city) => {
+    //       let cityID = city._id
+    //       return cityID.indexOf(
+    //        citiesFilter()) !== -1
+    //     })
+    //     this.setState({
+    //       filteredCities
+    //     })
+    //   }
 
 
 render() { 
     const itineraries = this.props.itineraries
-    // const id=this.props.match.params.cityID;
-    // const cities =this.props.cities;
+    const activities = this.props.activities
+    // let cityID = this.props.match.params.cityID
+    // const cities = this.props.cities
 
     return ( <div className="container">
         <NavBar/>
@@ -47,10 +64,10 @@ render() {
             <h3 className="text-center mb-4">Available Mytineraries:</h3>
             {itineraries.map(itinerary =>     
             <Fragment> 
-                <div> 
-                    {/* <img src={city.img} className="img-fluid cityImg" alt=" "/>
-                    <span><h2 className="overlayText">{city.name}</h2></span> */}
-                </div>
+                {/* <div> 
+                    {city.img && <img src={`${urlImages.urlImages}/images/cities/${city.img}`} className="img-fluid cityImg" alt=" "/>}
+                   <span><h2 className="overlayText">{city.name}</h2></span>
+                </div> */}
                 <ul className='nonStyleUl p-0'>
                 <li key={itinerary._id}>
                     <div className="row itineraryPrev bd-highlight m-0 mb-1 p-2 pb-0 container-fluid">
@@ -74,14 +91,11 @@ render() {
                             </div>
                         </div>
                     <div className="row col-xs-12 ml-0 pl-0 pr-0 mr-0 d-inline-block container-fluid">
-                        {/* {!this.state.clicked?( */}
                         <button id={itinerary._id} className="btn btn-light btn-block" onClick={()=>{this.toggle(itinerary._id)}}>View All</button>
-                        {/* // ): //"else" */}
-                        <Expand open={this.state.open==itinerary._id && this.state.clicked==true}>
-                            <Activities itinerary={itinerary} ></Activities>
-                            <button id={itinerary._id} className="btn btn-light btn-block" onClick={()=>{this.toggle('close')}}>Close</button> 
+                        <Expand open={this.state.open==itinerary._id && this.state.clicked==true}>            
+                            <Activities activities={activities}></Activities>
+                            <button id={itinerary._id} className="btn btn-light btn-block" onClick={()=>{this.toggle()}}>Close</button> 
                         </Expand>
-                        {/* } */}
                     </div>
                     </div>
                 </li>
@@ -100,7 +114,8 @@ const mapStateToProps = (state) => { // aca estoy pasando el state del storage c
     console.log(state);
     return {
         cities:state.citiesReducer.cities,
-        itineraries: state.itinerariesReducer.itineraries // aca le digo que va a tener una prop itineraries que va a ser manejada por X reducer
+        itineraries: state.itinerariesReducer.itineraries,
+        activities: state.activitiesReducer.activities 
     }
 }
 
